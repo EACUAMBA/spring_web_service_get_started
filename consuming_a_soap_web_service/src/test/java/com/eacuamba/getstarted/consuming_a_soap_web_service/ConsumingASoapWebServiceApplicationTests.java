4f30f70358b97f5e6d44039b772f382245eefb87
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-07-12 11:36:00 +0200

    Added the project to consume the ws and made some configurations in pom.xml

---
+++ consuming_a_soap_web_service/src/test/java/com/eacuamba/getstarted/consuming_a_soap_web_service/ConsumingASoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eacuamba.getstarted.consuming_a_soap_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingASoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
